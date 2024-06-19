c8f2e617da7e580a19549ca39448f7d506fd0adb
limlimlim-study <limlimlim.study@gmail.com>
2024-06-20 00:57:09 +0900

    chore: #4 scheduler service 생성

--- apps/kream-scraper/src/app.module.ts
+++ apps/kream-scraper/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SchedulerService } from './scheduler/scheduler.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, SchedulerService],
 })
 export class AppModule {}
