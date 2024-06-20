94cadb98470a6fa518338a42c40608cf49ad58cb
limlimlim-study <limlimlim.study@gmail.com>
2024-06-20 17:52:38 +0900

    feat: #4 스크랩 제품 확대 적용

--- apps/kream-scraper/src/scheduler/scheduler.service.ts
+++ apps/kream-scraper/src/scheduler/scheduler.service.ts
@@ -13,11 +13,64 @@ export class SchedulerService implements OnModuleInit {
   }
   @Cron(CronExpression.EVERY_30_MINUTES)
   async handleScrapCron() {
+    console.time('Complete all scrapes');
     const now = Date.now();
 
     await this.scraper.scrap(
       `https://kream.co.kr/exhibitions/${CategoryCode.MAN_SHOES}`,
       new ProductParser(new PageEvaluator(), Category.MAN_SHOES, now),
     );
+
+    await this.scraper.scrap(
+      `https://kream.co.kr/exhibitions/${CategoryCode.MAN_CLOTHES}`,
+      new ProductParser(new PageEvaluator(), Category.MAN_CLOTHES, now),
+    );
+
+    await this.scraper.scrap(
+      `https://kream.co.kr/exhibitions/${CategoryCode.WOMEN_SHOES}`,
+      new ProductParser(new PageEvaluator(), Category.WOMEN_SHOES, now),
+    );
+
+    await this.scraper.scrap(
+      `https://kream.co.kr/exhibitions/${CategoryCode.WOMEN_CLOTHES}`,
+      new ProductParser(new PageEvaluator(), Category.WOMEN_CLOTHES, now),
+    );
+
+    await this.scraper.scrap(
+      `https://kream.co.kr/exhibitions/${CategoryCode.WOMEN_SANDALS}`,
+      new ProductParser(new PageEvaluator(), Category.WOMEN_SANDALS, now),
+    );
+
+    await this.scraper.scrap(
+      `https://kream.co.kr/exhibitions/${CategoryCode.WISHS}`,
+      new ProductParser(new PageEvaluator(), Category.WISHS, now),
+    );
+
+    await this.scraper.scrap(
+      `https://kream.co.kr/exhibitions/${CategoryCode.HOT_STYLE}`,
+      new ProductParser(new PageEvaluator(), Category.HOT_STYLE, now),
+    );
+
+    await this.scraper.scrap(
+      `https://kream.co.kr/exhibitions/${CategoryCode.ACCESSORIES}`,
+      new ProductParser(new PageEvaluator(), Category.ACCESSORIES, now),
+    );
+
+    await this.scraper.scrap(
+      `https://kream.co.kr/exhibitions/${CategoryCode.BAGS}`,
+      new ProductParser(new PageEvaluator(), Category.BAGS, now),
+    );
+
+    await this.scraper.scrap(
+      `https://kream.co.kr/exhibitions/${CategoryCode.MAN_SANDALS}`,
+      new ProductParser(new PageEvaluator(), Category.MAN_SANDALS, now),
+    );
+
+    await this.scraper.scrap(
+      `https://kream.co.kr/exhibitions/${CategoryCode.WOMEN_SANDALS}`,
+      new ProductParser(new PageEvaluator(), Category.WOMEN_SANDALS, now),
+    );
+
+    console.timeEnd('Complete all scrapes');
   }
 }
