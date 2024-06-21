d487615ee20645bcc46c19b2817312fa6d198952
limlimlim-study <limlimlim.study@gmail.com>
2024-06-21 13:07:51 +0900

    리눅스운영체제 동작 테스트 19

--- apps/kream-scraper/src/scraper/scraper.service.ts
+++ apps/kream-scraper/src/scraper/scraper.service.ts
@@ -13,7 +13,7 @@ export class ScraperService {
   // private readonly userAgent =
   //   'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36';
   private readonly userAgent =
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36';
+    'Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.1 Mobile/15E148 Safari/604.1';
 
   async scrap(url: string, parser: ProductParser) {
     return this.scrapProducts(url, parser);
