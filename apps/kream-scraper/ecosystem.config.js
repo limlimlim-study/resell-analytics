413def56b0b4dc234fb3c681d4a41cd980f56903
limlimlim-study <limlimlim.study@gmail.com>
2024-06-22 01:47:57 +0900

    feat: #11 db 환경 분리 및 rds 사용 설정

---
+++ apps/kream-scraper/ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: 'kream-scraper',
+      script: 'dist/main.js',
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
--- apps/kream-scraper/src/app.module.ts
+++ apps/kream-scraper/src/app.module.ts
@@ -1,14 +1,23 @@
 import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SchedulerService } from './scheduler/scheduler.service';
 import { ScraperService } from './scraper/scraper.service';
 import { StoreService } from './store/store.service';
 import { PrismaService } from './prisma/prisma.service';
-
 @Module({
-  imports: [ScheduleModule.forRoot()],
+  imports: [
+    ScheduleModule.forRoot(),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath:
+        process.env.NODE_ENV === 'production'
+          ? '.env.production'
+          : '.env.development',
+    }),
+  ],
   controllers: [AppController],
   providers: [
     AppService,
