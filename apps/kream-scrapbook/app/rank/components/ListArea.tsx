a65260d038ac62df7f60cf2fddc816e9cc510f00
limlimlim-study <limlimlim.study@gmail.com>
2024-06-23 16:47:18 +0900

    feat: #8 검색 영역 UI 개발

---
+++ apps/kream-scrapbook/app/rank/components/ListArea.tsx
@@ -0,0 +1,8 @@
+import { Card } from "@/components/ui/card";
+import React from "react";
+
+const ListArea = () => {
+  return <Card className="p-3 h-full">ListArea</Card>;
+};
+
+export default ListArea;
---
+++ apps/kream-scrapbook/app/rank/components/SearchArea.tsx
@@ -0,0 +1,109 @@
+"use client";
+
+import { Card } from "@/components/ui/card";
+import React, { useEffect, useMemo, useState } from "react";
+import { DatePickerWithRange } from "./DateRangePicker";
+import { DateRange } from "react-day-picker";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Button } from "@/components/ui/button";
+
+const productCategories = [
+  {
+    name: "남성 신발 인기 순위",
+    code: "m_shoes",
+  },
+  {
+    name: "여성 신발 인기 순위",
+    code: "w_shoes",
+  },
+  {
+    name: "남성 의류 인기 순위",
+    code: "m_clothes",
+  },
+  {
+    name: "여성 의류 인시 순위",
+    code: "w_clothes",
+  },
+  {
+    name: "남성 샌들 인기 순위",
+    code: "m_sandals",
+  },
+  {
+    name: "여성 샌들 인기 순위",
+    code: "w_sandals",
+  },
+  {
+    name: "관심 상품 순위",
+    code: "wishs",
+  },
+  {
+    name: "스타일 최고 인기!",
+    code: "hot_style",
+  },
+  {
+    name: "액세서리 인기 순위 ",
+    code: "accessories",
+  },
+  {
+    name: "가방 인기 순위 ",
+    code: "bags",
+  },
+  {
+    name: "지금 많이 거래되는 상품",
+    code: "rising_sales",
+  },
+];
+
+const SearchArea = () => {
+  const [dateRange, setDateRange] = useState<DateRange>();
+  const [category, setCategory] = useState<string>();
+  const [enableSearch, setEnableSearch] = useState<boolean>();
+  const onClickSearch = () => {
+    console.log(category, dateRange);
+  };
+
+  const seelctItems = useMemo(() => {
+    return productCategories.map((category) => (
+      <SelectItem key={category.code} value={category.code}>
+        {category.name}
+      </SelectItem>
+    ));
+  }, []);
+
+  useEffect(() => {
+    setEnableSearch(!!(category && dateRange));
+  }, [category, dateRange]);
+
+  return (
+    <Card className="p-3 flex justify-between">
+      <div className="flex gap-3">
+        <Select onValueChange={setCategory}>
+          <SelectTrigger className="w-[210px]">
+            <SelectValue placeholder="카테고리" />
+          </SelectTrigger>
+          <SelectContent>{seelctItems}</SelectContent>
+        </Select>
+        <DatePickerWithRange
+          onSelectRange={(range: DateRange) => {
+            setDateRange(range);
+          }}
+        />
+      </div>
+      <Button
+        variant="default"
+        onClick={onClickSearch}
+        disabled={!enableSearch}
+      >
+        검색
+      </Button>
+    </Card>
+  );
+};
+
+export default SearchArea;
