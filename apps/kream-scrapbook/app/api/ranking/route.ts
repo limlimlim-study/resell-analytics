136a36cbcc30709202e2a26c21fe139519e4aa0c
limlimlim-study <limlimlim.study@gmail.com>
2024-06-24 18:52:33 +0900

    feat: #9 proxy gzip 설정

--- apps/kream-scrapbook/app/api/ranking/route.ts
+++ apps/kream-scrapbook/app/api/ranking/route.ts
@@ -1,10 +1,13 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
   const category = req.nextUrl.searchParams.get("category");
   const startTime = req.nextUrl.searchParams.get("startTime");
   const endTime = req.nextUrl.searchParams.get("endTime");
-  return await fetch(
+  const response = await fetch(
     `http://52.79.67.234:3001/scrap/${category}?startTime=${startTime}&endTime=${endTime}`
   );
+
+  const data = await response.json();
+  return NextResponse.json(data);
 }
