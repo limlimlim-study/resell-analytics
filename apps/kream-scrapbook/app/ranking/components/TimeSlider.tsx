1b9ddc434cd4ba64d82f5bd40f53eb0a2cd71f9f
limlimlim-study <limlimlim.study@gmail.com>
2024-06-25 00:38:04 +0900

    feat: #9 슬라이더 컴포넌트 교체

--- apps/kream-scrapbook/app/ranking/components/TimeSlider.tsx
+++ apps/kream-scrapbook/app/ranking/components/TimeSlider.tsx
@@ -1,17 +1,12 @@
 "use client";
 
-import {
-  ChakraProvider,
-  Slider,
-  SliderFilledTrack,
-  SliderMark,
-  SliderThumb,
-  SliderTrack,
-} from "@chakra-ui/react";
 import { useEffect, useMemo, useState } from "react";
-import useSearch from "../hooks/useSearch";
 import { format } from "date-fns";
+import Slider from "rc-slider";
+import useSearch from "../hooks/useSearch";
 import { Button } from "@/components/ui/button";
+
+import "rc-slider/assets/index.css";
 
 const TimeSlider = () => {
   const [sliderValue, setSliderValue] = useState(0);
@@ -28,22 +23,18 @@ const TimeSlider = () => {
   }, []);
 
   const sliderMarks = useMemo(() => {
-    return rankingData
-      .filter((data, i) => i % 2 === 1)
-      .map((data) => {
-        const date = new Date(data.key);
-        return (
-          <SliderMark
-            key={data.key}
-            textAlign="center"
-            value={date.getTime()}
-            {...labelStyles}
-          >
-            {format(date, "hh:mm")}
-          </SliderMark>
-        );
-      });
-  }, [labelStyles, rankingData]);
+    return rankingData.reduce((acc: any, item, i) => {
+      const date = new Date(item.key);
+      acc[item.value] = {
+        style: {
+          marginTop: 5,
+          transform: "rotate(45deg)",
+        },
+        label: i % 3 === 0 ? format(date, "hh:mm") : " ",
+      };
+      return acc;
+    }, {});
+  }, [rankingData]);
 
   useEffect(() => {
     if (!rankingData.length) return;
@@ -52,27 +43,21 @@ const TimeSlider = () => {
   }, [rankingData]);
 
   return (
-    <ChakraProvider>
-      <div className="flex gap-10 h-[100px] mt-[30px]">
-        <div className="w-full">
-          <Slider
-            aria-label="slider-ex-6"
-            onChange={(val) => setSliderValue(val)}
-            min={minMax[0]}
-            max={minMax[1]}
-            value={sliderValue}
-            isDisabled={rankingData.length === 0}
-          >
-            {sliderMarks}
-            <SliderTrack>
-              <SliderFilledTrack />
-            </SliderTrack>
-            <SliderThumb />
-          </Slider>
-        </div>
-        <Button disabled={rankingData.length === 0}>재생</Button>
+    <div className="flex gap-10 h-[100px] mt-[30px]">
+      <div className="w-full">
+        <Slider
+          dots
+          min={minMax[0]}
+          max={minMax[1]}
+          step={null}
+          marks={sliderMarks}
+          onChange={(e) => {
+            console.log(e);
+          }}
+        />
       </div>
-    </ChakraProvider>
+      <Button disabled={rankingData.length === 0}>재생</Button>
+    </div>
   );
 };
 
