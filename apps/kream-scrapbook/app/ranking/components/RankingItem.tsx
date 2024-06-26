9cf6b48d4b1943681df2da747ddfb1fbb8a4ea60
limlimlim-study <limlimlim.study@gmail.com>
2024-06-26 17:25:27 +0900

    feat: #9 이미지 프록시 제거

--- apps/kream-scrapbook/app/ranking/components/RankingItem.tsx
+++ apps/kream-scrapbook/app/ranking/components/RankingItem.tsx
@@ -1,17 +1,28 @@
 import { Card } from "@/components/ui/card";
 import Image from "next/image";
+import { useState } from "react";
 
 interface Props {
-  rank?: number;
   data: Ranker;
 }
 
 const RankingItem = ({ data }: Props) => {
+  console.log(data.image);
+  const [imageUrl] = useState(() => {
+    const imageUrl = data.image;
+    const smallImage = imageUrl.replace("type=m", "type=s");
+    return smallImage;
+  });
+
   return (
-    <Card className="h-[55px] w-[55px] mb-[2px]">
+    <Card className="h-[55px] w-[55px] mb-[2px] relative overflow">
+      <div className="w-[20px] h-[20px] rounded-full absolute bg-slate-100 border-slate-800 text-[10px] text-slate-800 left-[-5px] top-[-5px] flex justify-center items-center font-bold">
+        <div>{data.rank}</div>
+      </div>
       <Image
-        src={`/api/image?url=${data.image}`}
-        alt="test"
+        src={imageUrl}
+        alt="product"
+        loading="lazy"
         width={50}
         height={50}
       />
