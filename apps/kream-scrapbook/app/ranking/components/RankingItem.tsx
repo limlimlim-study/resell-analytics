43907f9e1194fdfa04c2ca5d3639169e232c17e8
limlimlim-study <limlimlim.study@gmail.com>
2024-07-01 01:04:55 +0900

    이미지 효과

--- apps/kream-scrapbook/app/ranking/components/RankingItem.tsx
+++ apps/kream-scrapbook/app/ranking/components/RankingItem.tsx
@@ -8,6 +8,7 @@ interface Props {
 }
 
 const RankingItem = ({ data }: Props) => {
+  const [loaded, setLoaded] = useState(false);
   const [imageUrl] = useState(() => {
     const imageUrl = data.image;
     const smallImage = imageUrl.replace("type=m", "type=s");
@@ -16,15 +17,15 @@ const RankingItem = ({ data }: Props) => {
 
   return (
     <Card
-      className="h-[65px] w-[65px] relative overflow hover:shadow-lg transition-shadow duration-300 cursor-pointer"
+      className="h-[65px] w-[65px] relative overflow hover:shadow-lg transition-shadow duration-500 ease-in-out cursor-pointer"
       onClick={() => {
         window.open(data.url);
       }}
     >
       <div
-        className="w-[20px] h-[20px] rounded-full absolute bg-slate-100 border-slate-800 text-[10px] text-slate-800 left-[-5px] top-[-5px] flex justify-center items-center font-bold"
-        data-tooltip-id="product-tooltip"
-        data-tooltip-content="Hello world!"
+        className={`${
+          loaded ? "opacity-100" : "opacity-0"
+        } transition-opacity duration-1000 ease-in-out w-[20px] h-[20px] rounded-full absolute bg-slate-100 border-slate-800 text-[10px] text-slate-800 left-[-5px] top-[-5px] flex justify-center items-center font-bold`}
       >
         <div>{data.rank}</div>
       </div>
@@ -34,6 +35,9 @@ const RankingItem = ({ data }: Props) => {
         loading="lazy"
         width={100}
         height={100}
+        onLoad={() => {
+          setLoaded(true);
+        }}
       />
     </Card>
   );
