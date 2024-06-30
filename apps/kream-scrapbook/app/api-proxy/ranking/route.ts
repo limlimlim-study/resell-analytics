a4fbc2d972558491455789884f267162aa1bbb2b
limlimlim-study <limlimlim.study@gmail.com>
2024-06-30 16:10:46 +0900

    chore: api log

--- apps/kream-scrapbook/app/api-proxy/ranking/route.ts
+++ apps/kream-scrapbook/app/api-proxy/ranking/route.ts
@@ -4,6 +4,9 @@ export async function GET(req: NextRequest) {
   const category = req.nextUrl.searchParams.get("category");
   const startTime = req.nextUrl.searchParams.get("startTime");
   const endTime = req.nextUrl.searchParams.get("endTime");
+  console.log(
+    `http://52.79.245.87/api/scrap/${category}?startTime=${startTime}&endTime=${endTime}`
+  );
   const response = await fetch(
     `http://52.79.245.87/api/scrap/${category}?startTime=${startTime}&endTime=${endTime}`
   );
